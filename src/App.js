e5eb8696983cb24534dfffeb6ae2b2ac8a1950cc
pchessah <paulchesa1@gmail.com>
2020-01-07 10:40:27 +0300

    rearrange files

--- src/App.js
+++ src/App.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './App.css';
-import Home from "./components/Home";
-import Aboutus from "./components/Aboutus";
-import Shop from "./components/Shop";
-import Drink from "./components/Drink";
-import Others from "./components/Others";
-import Navbar from "./components/navbar";
-import Food from "./components/Food";
-import SignupCustomer from "./components/signupCustomer";
-import LoginCustomer from "./components/LoginCustomer"
-import SignupShopOwner from './components/SignupShopOwner';
-import LoginShopOwner from './components/LoginShopOwner';
-import SignupCourier from './components/SignupCourier';
-import LoginCourier from './components/LoginCourier';
-import FoodShop from './components/FoodShop';
-import OtherShops from './components/OtherShops';
-import ShopHome from './components/ShopHome';
+import Home from "./components/client/Home";
+import Aboutus from "./components/client/Aboutus";
+import Shop from "./components/client/Shop";
+import Drink from "./components/client/Drink";
+import Others from "./components/client/Others";
+import Navbar from "./components/client/navbar";
+import Food from "./components/client/Food";
+import SignupCustomer from "./components/client/signupCustomer";
+import LoginCustomer from "./components/client/LoginCustomer"
+import SignupShopOwner from './components/client/SignupShopOwner';
+import LoginShopOwner from './components/client/LoginShopOwner';
+import SignupCourier from './components/client/SignupCourier';
+import LoginCourier from './components/client/LoginCourier';
+import FoodShop from './components/client/FoodShop';
+import OtherShops from './components/client/OtherShops';
+import ShopHome from './components/client/ShopHome';
+import ShopOwnerDashboard from './components/client/ShopOwnerDashboard';
 import { Switch, BrowserRouter as Router, Route } from "react-router-dom";
 
 function App() {
@@ -38,6 +39,7 @@ function App() {
             <Route path="/courierlogin" component={LoginCourier}/>
             <Route path="/fooddrinkshop" component={FoodShop}/>
             <Route path="/othersshop" component={OtherShops}/>
+            <Route path="/ownerdashboard" component={ShopOwnerDashboard}/>
         </Switch>
       </Router>
     </div>
---
+++ src/components/client/ShopOwnerDashboard.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const ShopOwnerDashboard = () => {
+    return (
+        <div>
+            <h1> Dashboard </h1>
+        </div>
+    )
+}
+
+export default ShopOwnerDashboard;
