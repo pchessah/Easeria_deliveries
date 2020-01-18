54130281f0439a811078d508eb93d6e566f02ec9
Paul Chesa <paulchesa1@gmail.com>
2020-01-18 17:04:02 +0300

    Update courier.js

--- server/routes/courier.js
+++ server/routes/courier.js
@@ -1,10 +1,17 @@
 const router = require("express").Router();
 const { Courier, Customer, ShopOwner, Logins, ProductSchema} = require("../models/data")
+const path = require ("path")
 
-router.post("/addproduct", (req, res)=>{
+
+router.post("/addproduct", (req, res, next)=>{
     const {productName, productPrice, category, description, image } = req.body;
+
+    image.mv(path.join(__dirname,"/uploads", image.name))
+    .then(()=>console.log("success chessah"))
+    .catch(err=>console.log(err.stack))
     
-    console.log(req.body)
+    console.log(image)
+   
 
 })
 
