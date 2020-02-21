f774dafe01b57da1b4c92fc183dbf2a1230a02ad
pchessah <paulchesa1@gmail.com>
2020-02-21 16:21:09 +0300

    increase responsiveness

--- src/components/client/products/products.js
+++ src/components/client/products/products.js
@@ -1,7 +1,47 @@
 import React from "react";
+import {
+  Card,
+  Button,
+  CardImg,
+  CardTitle,
+  CardText,
+  CardGroup,
+  CardSubtitle,
+  CardBody
+} from "reactstrap";
+
+import productData from "../products/products.json";
 
 function Products() {
-  return <div>Cows will fly project</div>;
+  return (
+    <div className="container">
+      <div className="row">
+        {productData.map((value, indx) => {
+          return (
+        <CardGroup className="col-xm-12 col-sm-6 col-md-4 col-lg-3">
+            <Card className="product">
+              <CardImg
+                top
+                width="100%"
+                src="/assets/318x180.svg"
+                alt="Card image cap"
+              />
+              <CardBody>
+                <CardTitle>{value.name}</CardTitle>
+                <CardSubtitle>Card subtitle</CardSubtitle>
+                <CardText>
+                  This card has supporting text below as a natural lead-in to
+                  additional content.
+                </CardText>
+                <Button>Button</Button>
+              </CardBody>
+            </Card>
+      </CardGroup>
+          );
+        })}
+        </div>
+    </div>
+  );
 }
 
 export default Products;
