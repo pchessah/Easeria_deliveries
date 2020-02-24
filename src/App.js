45896a613dfd101b4a9f7df1bc501ae1d779a13e
pchessah <paulchesa1@gmail.com>
2020-02-24 17:11:59 +0300

    product page added

--- src/App.js
+++ src/App.js
@@ -4,18 +4,18 @@ import { Router, Route, Switch, Redirect } from "react-router-dom";
 import PublicRoutes from "./routes/publicRoutes";
 import PrivateRoutes from "./routes/privateRoutes";
 import history from "./util/history";
-
+import "react-toastify/dist/ReactToastify.min.css";
+import { ToastContainer} from 'react-toastify';
 
 const App = () => (
-  <div  style={{maxWidth:"100vw"}}>
-
-      <Router history={history}>
-        <Switch>
-          <Route path="/app" component={PrivateRoutes} />
-          <Route path="" component={PublicRoutes} />
-        </Switch>
-      </Router>
-
+  <div style={{ maxWidth: "100vw" }}>
+    <Router history={history}>
+      <ToastContainer autoClose={1500}/>
+      <Switch>
+        <Route path="/app" component={PrivateRoutes} />
+        <Route path="" component={PublicRoutes} />
+      </Switch>
+    </Router>
   </div>
 );
 
--- src/components/client/products/products.js
+++ src/components/client/products/products.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import {
   Card,
   Button,
@@ -10,35 +10,89 @@ import {
   CardBody
 } from "reactstrap";
 
+import { toast } from "react-toastify";
+
 import productData from "../products/products.json";
 
-function Products() {
+function Products(props) {
+
+  const initialCart =() => JSON.parse(localStorage.getItem("productData"))
+  const [data, setData] = useState();
+  const [cartProduct, setCartProduct] = useState([...initialCart]);
+
+  useEffect(() => {
+    setData(productData);
+  }, []);
+
+  console.log(initialCart());
+  
+  useEffect(()=>{
+    // set the browser stoarge with the cart products
+    initialCart()
+      const json = JSON.stringify(cartProduct);
+      localStorage.setItem("productData", json);
+  },[cartProduct])
+
+  // useEffect(()=>{
+  //     try {
+  //       const json = localStorage.getItem("productData");
+  //       const parsedData = JSON.parse(json);
+  //       console.log(parsedData);
+
+  //       if (parsedData) {
+  //         setCartProduct(parsedData);
+  //       }
+  //     } catch (e) {
+  //       console.log("Unable to pass data");
+  //     }
+
+  // },[])
+
+  const handleAddCart = indx => {
+    if (JSON.parse(localStorage.getItem("productData")).includes(data[indx])) {
+      toast.error("Item already in cart");
+    } else {
+      setCartProduct([...cartProduct, data[indx]]);
+      toast.success("successfully added");
+    }
+  };
+  // console.log(cartProduct);
+  const json = localStorage.getItem("productData");
+  const parsedData = JSON.parse(json);
+
+  console.log(parsedData);
+
   return (
     <div className="container">
       <div className="row">
-        {productData.map((value, indx) => {
-          return (
-        <CardGroup className="col-xm-12 col-sm-6 col-md-4 col-lg-3">
-            <Card className="product">
-              <CardImg
-                top
-                width="100%"
-                src={value.image}
-                alt="Card image cap"
-              />
-              <CardBody>
-                <CardTitle>{value.name}</CardTitle>
-                <CardSubtitle>Ksh {value.price}</CardSubtitle>
-                <CardText>
-               {value.description}
-                </CardText>
-                <Button>Add to cart</Button>
-              </CardBody>
-            </Card>
-      </CardGroup>
-          );
-        })}
-        </div>
+        {data &&
+          data.map((productValue, indx) => {
+            return (
+              <CardGroup className="col-xm-12 col-sm-6 col-md-4 col-lg-3">
+                <Card className="product">
+                  <CardImg
+                    className="photo"
+                    top
+                    width="100%"
+                    src={productValue.image}
+                    alt="Card image cap"
+                  />
+                  <CardBody>
+                    <CardTitle>{productValue.name}</CardTitle>
+                    <CardSubtitle>Ksh {productValue.price}</CardSubtitle>
+                    <CardText>{productValue.description}</CardText>
+                    {/* <button className="calculate">+</button>
+                <button className="calculate">-</button> */}
+                    <br />
+                    <Button onClick={() => handleAddCart(indx)}>
+                      Add to cart
+                    </Button>
+                  </CardBody>
+                </Card>
+              </CardGroup>
+            );
+          })}
+      </div>
     </div>
   );
 }
