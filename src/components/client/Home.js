6f547f8a53d76e0ba78fa3a1474209982936c3eb
pchessah <paulchesa1@gmail.com>
2020-03-11 12:02:32 +0300

    Add card for deliveries

--- src/components/client/Home.js
+++ src/components/client/Home.js
@@ -1,80 +1,108 @@
-import React from 'react';
-import { UncontrolledCarousel } from 'reactstrap';
+import React from "react";
+import {
+  UncontrolledCarousel,
+  InputGroup,
+  Button,
+  Input,
+  Card
+} from "reactstrap";
 
 const items = [
   {
     src:
-      'https://images.pexels.com/photos/616404/pexels-photo-616404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    altText: 'Slide 1',
-    caption: 'Get your goods fast!',
-    header: 'Easeria Deliveries',
-    key: '1',
+      "https://images.pexels.com/photos/616404/pexels-photo-616404.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    altText: "Slide 1",
+    caption: "Get your goods fast!",
+    header: "Easeria Deliveries",
+    key: "1"
   },
   {
     src:
-      'https://images.pexels.com/photos/2159065/pexels-photo-2159065.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    altText: 'Slide 2',
-    caption: '',
-    header: 'Affordable prices',
-    key: '2',
+      "https://images.pexels.com/photos/2159065/pexels-photo-2159065.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    altText: "Slide 2",
+    caption: "",
+    header: "Affordable prices",
+    key: "2"
   },
   {
     src:
-      'https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940',
-    altText: 'Slide 3',
-    caption: 'Great customer experience',
-    header: 'Trusted Vendors',
-    key: '3',
-  },
+      "https://images.pexels.com/photos/264636/pexels-photo-264636.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940",
+    altText: "Slide 3",
+    caption: "Great customer experience",
+    header: "Trusted Vendors",
+    key: "3"
+  }
 ];
-const Home = (props) => (
-  <div>
-    
-    <UncontrolledCarousel
-      className="homeCarousel"
-      style={{ height: '50vh' }}
-      items={items}
-    />
-    <div className="row contain mx-auto">
-      <a
-        href="/food"
-        className="col-md-3 categories  tint"
+
+function Home() {
+  return (
+    <div>
+      <Card
         style={{
-          background:
-              'url(https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)',
+          width: "500px",
+          padding: "10px",
+          position: "fixed",
+          zIndex: "2",
+          right: "5px"
         }}
       >
-        <div align="center">
-          <h3>Food</h3>
-        </div>
-      </a>
-      <a
-        href="/drink"
-        className="col-md-3 categories tint"
-        style={{
-          background:
-              'url(https://images.pexels.com/photos/605408/pexels-photo-605408.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)',
-        }}
-      >
-        <div align="center">
-          <h3>Drinks</h3>
-        </div>
-      </a>
-      <a
-        href="/others"
-        className="col-md-3 categories tint"
-        style={{
-          background:
-              'url(https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)',
-        }}
-      >
-        <div align="center">
-          <h3>Others</h3>
-        </div>
-      </a>
+        <InputGroup>
+          <Input placeholder="enter pickup location" />
+        </InputGroup>{" "}
+        <br />
+        <InputGroup>
+          <Input placeholder="enter destination location" />
+        </InputGroup>
+        <hr />
+        <Button color="secondary"> Confirm Order </Button>
+      </Card>
+
+      <UncontrolledCarousel
+        className="homeCarousel"
+        style={{ height: "50vh" }}
+        items={items}
+      />
+      <div className="row contain mx-auto">
+        <a
+          href="/food"
+          className="col-md-3 categories  tint"
+          style={{
+            background:
+              "url(https://images.pexels.com/photos/262959/pexels-photo-262959.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)"
+          }}
+        >
+          <div align="center">
+            <h3>Food</h3>
+          </div>
+        </a>
+        <a
+          href="/drink"
+          className="col-md-3 categories tint"
+          style={{
+            background:
+              "url(https://images.pexels.com/photos/605408/pexels-photo-605408.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)"
+          }}
+        >
+          <div align="center">
+            <h3>Drinks</h3>
+          </div>
+        </a>
+        <a
+          href="/others"
+          className="col-md-3 categories tint"
+          style={{
+            background:
+              "url(https://images.pexels.com/photos/322207/pexels-photo-322207.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500)"
+          }}
+        >
+          <div align="center">
+            <h3>Others</h3>
+          </div>
+        </a>
+      </div>
+      {/* <Footer/> */}
     </div>
-    {/* <Footer/> */}
-  </div>
-);
+  );
+}
 
 export default Home;
