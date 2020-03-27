d849267eb7838460ff14a8123c777254533504e3
pchessah <paulchesa1@gmail.com>
2020-03-27 16:16:13 +0300

    add css

--- src/components/client/customer/checkout/FinalConfirm.js
+++ src/components/client/customer/checkout/FinalConfirm.js
@@ -1,11 +1,34 @@
-import React from 'react';
+import React, { useState } from "react";
+import { Jumbotron, Button, Fade } from "reactstrap";
 
-function FinalConfirm() {
-    return (
-        <div>
-            Confirm order
-        </div>
-    )
+function FinalConfirm(props) {
+  const [fadeIn, setFadeIn] = useState(false);
+
+  const toggle = () => setFadeIn(!fadeIn);
+  return (
+    <div>
+      <Button color="primary" onClick={toggle}>
+        Toggle Fade
+      </Button>
+      <Fade in={fadeIn} tag="h5" className="mt-3">
+        <Jumbotron>
+          <h1 className="display-3">Hello, world!</h1>
+          <p className="lead">
+            This is a simple hero unit, a simple Jumbotron-style component for
+            calling extra attention to featured content or information.
+          </p>
+          <hr className="my-2" />
+          <p>
+            It uses utility classes for typography and spacing to space content
+            out within the larger container.
+          </p>
+          <p className="lead">
+            <Button color="primary">Learn More</Button>
+          </p>
+        </Jumbotron>
+      </Fade>
+    </div>
+  );
 }
 
 export default FinalConfirm;
